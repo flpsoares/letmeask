ea4c31430a8b39aaf904ba03ec45293d6193f435
Filipe Soares <filipeseventeen1@gmail.com>
2021-06-21 00:53:21 -0300

    Adicionado ESLint e Prettier ao projeto

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,7 +3,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
